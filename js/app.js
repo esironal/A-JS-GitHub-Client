ec0ed5c7a00a6e5b9e1c2bb82c292217e2170fc9
Craig J <craigdanj@gmail.com>
2015-01-09 23:29:56 +0530

    Routing code added to app.js
    Code added to app.js to handle routing. Code login will also now be
    separated into different controllers.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
-angular.module('GithubClient', []).
-    controller('LoginController', ['$scope', '$http', function ($scope, $http) {
+angular.module('GithubClient', ['ngRoute'])
+    
+    .config(function($routeProvider) {
+        
+        $routeProvider
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: 'partials/login.html'    
+            })
+            .when('/profile', {
+                controller: 'ProfileController',
+                templateUrl: 'partials/profile.html'
+            })
+    })
+
+    .controller('LoginController', ['$scope', '$http', function ($scope, $http) {
 
         $scope.profile_data;
 
         $scope.ViewProfile = function() {
 
-            $http.get("https://api.github.com/users/" + $scope.username).
-            success( function (data, status, headers, config) {
+            $http.get("https://api.github.com/users/" + $scope.username)
+                
+            .success( function (data, status, headers, config) {
 
                 $scope.profile_data = data;
 
@@ -16,12 +31,17 @@ angular.module('GithubClient', []).
                 document.write('Public Gists:' + data.public_gists + "<br />");
 
                 console.log(status);
-            }).
-            error( function (data, status, headers, config) {
+            })
+            
+            .error( function (data, status, headers, config) {
                 document.write(status);
                 console.log(status);
             });
 
         };
+
+    }])
+
+    .controller('ProfileController', ['$scope', '$http', function ($scope, $http) {
 
     }]);
