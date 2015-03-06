441c2ca25de7d68fecf1187ae047ff6d335096d6
Craig J <craigdanj@gmail.com>
2015-03-07 00:32:54 +0530

    Code to handle profile url changes added.
    Code to fetch profile when username in url changes or when profile page
    is refreshed was added o app.js

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ angular.module('GithubClient', ['ngRoute'])
     .controller('LoginController', function ($scope, $http, $location, ProfileData) {
         
         //Fetch Profile Data
-        //------------------------------------
+        //------------------
         $scope.FetchProfileData = function() {
 
             //Reset the request-error string
@@ -114,23 +114,57 @@ angular.module('GithubClient', ['ngRoute'])
     // Profile page
     //======================
 
-    .controller('ProfileController', function ($scope, $http, $location, ProfileData) {
+    .controller('ProfileController', function ($scope, $http, $location, $routeParams, ProfileData) {
         
         $scope.profile_data = ProfileData.getData();
         $scope.username = ProfileData.getUser();
-
-        $scope.callFetchMethod = function (message) {
-            console.log(message);
-        }
         
-        if(Object.getOwnPropertyNames($scope.profile_data).length === 0) {
-            $scope.callFetchMethod("No profile data");
-        } else {
-            $scope.callFetchMethod("Yep we gots the stuff");
 
+        //Code to fetch the Profile Data if the profile page is refreshed
+        //---------------------------------------------------------------
+        $scope.FetchProfileData = function() {
+
+            //Save username in service
+            ProfileData.setUser($scope.username);
+
+
+            $http.get("https://api.github.com/users/" + $scope.username)
+
+            .success( function (data, status, headers, config) {
+
+                $scope.profile_data = data;
+
+                ProfileData.setData(data);
+
+                $location.path('/profile/' + $scope.username);
+
+                console.log(data, status);
+
+            })
+
+            .error( function (data, status, headers, config) {
+                console.log(data, status, "profile fetch error", headers);
+                $scope.profileRequestError = "Error Fetching profile info.";
+                
+                //$location.path("/");
+            });
+
+        };
+    
+        
+        //Check whether the same user-profile needs to be fetched again or whether a new profile needs to be fetched    
+        //----------------------------------------------------------------------------------------------------------
+        if( $scope.username == "" ) {
+            $scope.username = $routeParams.username;
+            $scope.FetchProfileData();
+        } else if( $scope.username !== $routeParams.username ) {
+            $scope.username = $routeParams.username;
+            $scope.FetchProfileData();
         }
 
-        
+
+        //Code to fetch the repository list
+        //---------------------------------
         $scope.ShowRepositoryList = function() {
 
             console.log($scope.profile_data);
